0d7046a6afce87f508dca78a292c93ab2a55d0d7
Blublac <kenechukwuc7@gmail.com>
2021-12-23 18:39:22 +0100

    updates

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="PHISTON",
+        title="FOODIE-Z",
         default_version = "v1",
         description = "Food ordering api, customers place order and vendors supply.",
         terms_of_service="",
